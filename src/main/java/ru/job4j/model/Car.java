8378c8b1ef1270d4607f9b3799af6c224000412b
Tsypa90 <URSA2004@mail.ru>
2022-08-12 19:17:13 +0300

    Модели и связи. Машины и владельцы [#4744]

---
+++ src/main/java/ru/job4j/model/Car.java
@@ -0,0 +1,66 @@
+package ru.job4j.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Table(name = "car")
+public class Car {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @OneToOne
+    @JoinColumn(name = "engine_id")
+    private Engine engine;
+    @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Driver> drivers = new HashSet<>();
+
+    public static Car of(Engine engine) {
+        Car car = new Car();
+        car.engine = engine;
+        return car;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public Set<Driver> getDrivers() {
+        return drivers;
+    }
+
+    public void setDrivers(Set<Driver> drivers) {
+        this.drivers = drivers;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        Car car = (Car) o;
+        return id == car.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
