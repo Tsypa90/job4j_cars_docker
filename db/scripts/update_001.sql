5fc8c0e8b115b05740588adb7f1c004f0f4456af
Tsypa90 <URSA2004@mail.ru>
2022-08-13 12:59:37 +0300

    Реализовать площадку продаж машин. [#4747 #331788]

--- db/scripts/update_001.sql
+++ db/scripts/update_001.sql
@@ -18,6 +18,12 @@ create table if not exists history_owner (
     car_id int not null references car(id)
 );
 
+create table if not exists users (
+	id serial primary key,
+	name varchar(255) unique,
+	password varchar(255) not null
+);
+
 create table if not exists advt (
 	id serial primary key,
 	descrp varchar(255),
@@ -26,10 +32,4 @@ create table if not exists advt (
 	photo bytea,
 	saled boolean,
 	user_id int not null references users(id)
-);
-
-create table if not exists users (
-	id serial primary key,
-	name varchar(255) unique,
-	password varchar(255) not null
 );
